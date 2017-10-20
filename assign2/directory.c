b75c5246697c26fd15b582960e6aa608d411a6be
Chuiwen Ma <machuiwen@gmail.com>
2017-10-20 22:34:42 +0000

    Finish assignment 2

--- assign2/directory.c
+++ assign2/directory.c
@@ -8,6 +8,11 @@
 
 int directory_findname(struct unixfilesystem *fs, const char *name,
                        int dirinumber, struct direntv6 *dirEnt) {
+  int nameLen = strlen(name);
+  if (nameLen > 14) {
+    fprintf(stderr, "file name greater than 14: %s\n", name);
+    return -1;
+  }
   struct inode dirInode;
   if (inode_iget(fs, dirinumber, &dirInode) == -1) {
     fprintf(stderr, "error occurred when calling inode_iget.\n");
@@ -17,6 +22,22 @@ int directory_findname(struct unixfilesystem *fs, const char *name,
     fprintf(stderr, "the given inode %d is not a directory.\n", dirinumber);
     return -1;
   }
-  fprintf(stderr, "directory_lookupname(name=%s dirinumber=%d)\n", name, dirinumber);
-  return -1;
+  int dirSize = inode_getsize(&dirInode);
+  if (dirSize == 0) return -1; // empty directory
+  char dirBuffer[dirSize];
+  int numBlocks = dirSize / DISKIMG_SECTOR_SIZE + ((dirSize % DISKIMG_SECTOR_SIZE != 0) ? 1 : 0);
+  for (int i = 0; i < numBlocks; i++) {
+    int numValidBytes = file_getblock(fs, dirinumber, i, dirBuffer + i * DISKIMG_SECTOR_SIZE);
+    if (numValidBytes == -1) return -1;
+  }
+  size_t dirEntSize = sizeof(struct direntv6);
+  int numDirEnt = dirSize / dirEntSize;
+  for (int i = 0; i < numDirEnt; i++) {
+    char *dirEntP = dirBuffer + dirEntSize * i;
+    if (memcmp(dirEntP + sizeof(uint16_t), name, nameLen) == 0) {
+      memcpy(dirEnt, dirEntP, dirEntSize);
+      return 0;
+    }
+  }
+  return -1; // not found
 }
--- assign2/pathname.c
+++ assign2/pathname.c
@@ -1,14 +1,42 @@
-
 #include "pathname.h"
 #include "directory.h"
 #include "inode.h"
 #include "diskimg.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <assert.h>
 
+int pathname_lookup_recursive(struct unixfilesystem *fs, const char *pathname, int dirinumber) {
+  if (strlen(pathname) == 0 || (*pathname) != '/') {
+    fprintf(stderr, "wrong pathname syntax:%s\n", pathname);
+    return -1;
+  } else if (strlen(pathname) == 1) {
+    return dirinumber;
+  }
+  char *nextSlash = strchr(pathname + 1, '/');
+  char *dirEntName;
+  if (nextSlash) {
+    size_t dirEntNameLen = nextSlash - pathname;
+    dirEntName = (char *) malloc(dirEntNameLen);
+    assert(dirEntName != NULL);
+    memcpy(dirEntName, pathname + 1, dirEntNameLen);
+    dirEntName[dirEntNameLen - 1] = '\0';
+  } else {
+    dirEntName = pathname + 1;
+  }
+  struct direntv6 dirEnt;
+  if (directory_findname(fs, dirEntName, dirinumber, &dirEnt) < 0) {
+    return -1;
+  }
+  if (nextSlash) {
+    free(dirEntName);
+    return pathname_lookup_recursive(fs, nextSlash, dirEnt.d_inumber);
+  } else {
+    return dirEnt.d_inumber;
+  }
+}
+
 int pathname_lookup(struct unixfilesystem *fs, const char *pathname) {
-  // remove the placeholder implementation and replace with your own
-  fprintf(stderr, "pathname_lookup(path=%s) unimplemented.  Returing -1.\n", pathname);
-  return -1;
+  return pathname_lookup_recursive(fs, pathname, 1);
 }
