84da14435d14e084d690b45660683a7b80083915
Chuiwen Ma <chuiwen.ma@oracle.com>
2017-10-19 21:45:53 -0700

    Start on directory.c

--- assign2/directory.c
+++ assign2/directory.c
@@ -6,9 +6,17 @@
 #include <string.h>
 #include <assert.h>
 
-// remove the placeholder implementation and replace with your own
 int directory_findname(struct unixfilesystem *fs, const char *name,
-		       int dirinumber, struct direntv6 *dirEnt) {
-  fprintf(stderr, "directory_lookupname(name=%s dirinumber=%d)\n", name, dirinumber); 
+                       int dirinumber, struct direntv6 *dirEnt) {
+  struct inode dirInode;
+  if (inode_iget(fs, dirinumber, &dirInode) == -1) {
+    fprintf(stderr, "error occurred when calling inode_iget.\n");
+    return -1;
+  }
+  if ((dirInode.i_mode & IFMT) != IFDIR) {
+    fprintf(stderr, "the given inode %d is not a directory.\n", dirinumber);
+    return -1;
+  }
+  fprintf(stderr, "directory_lookupname(name=%s dirinumber=%d)\n", name, dirinumber);
   return -1;
 }
