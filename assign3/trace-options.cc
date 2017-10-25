f783582ba5c2fb11e5370a41fbce606b357dad48
Chuiwen Ma <machuiwen@gmail.com>
2017-10-25 08:28:27 +0000

    Fix trace-options.cc

--- assign3/trace-options.cc
+++ assign3/trace-options.cc
@@ -6,14 +6,22 @@
 
 #include "trace-options.h"
 #include <string>
-//#include "string-utils.h"
+#include <string.h>
+// #include "string-utils.h"
 using namespace std;
+
+// Implement startwWith due to lack of string-utils.h
+bool startsWith(const char *s1, const char *s2) {
+  size_t s1len = strlen(s1);
+  size_t s2len = strlen(s2);
+  return s1len < s2len ? false : strncmp(s1, s2, s2len) == 0;
+}
 
 static const string kSimpleFlag = "--simple";
 static const string kRebuildFlag = "--rebuild";
 size_t processCommandLineFlags(bool& simple, bool& rebuild, char *argv[]) throw (TraceException) {  
   size_t numFlags = 0;
-  for (int i = 1; argv[i] != NULL; i++) {
+  for (int i = 1; argv[i] != NULL && startsWith(argv[i], "--"); i++) {
     if (argv[i] == kSimpleFlag) simple = true;
     else if (argv[i] == kRebuildFlag) rebuild = true;
     else throw TraceException(string(argv[0]) + ": Unrecognized flag (" + argv[i] + " )");
