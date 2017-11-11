d1c01cbba5d9b7d9fb15f40050c18aeb650c4d17
Chuiwen Ma <machuiwen@gmail.com>
2017-11-11 08:14:34 +0000

    Finish fg built-in

--- assign4/stsh.cc
+++ assign4/stsh.cc
@@ -19,9 +19,19 @@
 #include <unistd.h>  // for fork
 #include <signal.h>  // for kill
 #include <sys/wait.h>
+#include <cassert>
 using namespace std;
 
 static STSHJobList joblist; // the one piece of global data we need so signal handlers can access it
+
+static void updateJobList(STSHJobList& jobList, pid_t pid, STSHProcessState state) {
+  if (!jobList.containsProcess(pid)) return;
+  STSHJob& job = jobList.getJobWithProcess(pid);
+  assert(job.containsProcess(pid));
+  STSHProcess& process = job.getProcess(pid);
+  process.setState(state);
+  jobList.synchronize(job);
+}
 
 /**
  * Function: handleBuiltin
@@ -32,6 +42,55 @@ static STSHJobList joblist; // the one piece of global data we need so signal ha
  */
 static const string kSupportedBuiltins[] = {"quit", "exit", "fg", "bg", "slay", "halt", "cont", "jobs"};
 static const size_t kNumSupportedBuiltins = sizeof(kSupportedBuiltins)/sizeof(kSupportedBuiltins[0]);
+
+static void waitForFgJobToFinish() {
+  sigset_t additions, existingmask;
+  sigemptyset(&additions);
+  sigaddset(&additions, SIGCHLD);
+  sigprocmask(SIG_BLOCK, &additions, &existingmask);
+  while (joblist.hasForegroundJob()) {
+    sigsuspend(&existingmask);
+  }
+  sigprocmask(SIG_UNBLOCK, &additions, NULL);
+}
+
+static void fg(const pipeline& pipeline) {
+  string usage = "Usage: fg <jobid>.";
+  command cmd = pipeline.commands[0];
+  size_t jobnum;
+  // input sanity check
+  if (cmd.tokens[0] == NULL || cmd.tokens[1] != NULL) {
+    throw STSHException(usage);
+  } else {
+    try {
+      string jobid = string(cmd.tokens[0]);
+      size_t endpos;
+      long long num = stoll(jobid, &endpos);
+      if (endpos != jobid.size() || num < 0) {
+        throw STSHException(usage);
+      }
+      jobnum = (size_t) num;
+    } catch (...) {
+      throw STSHException(usage);
+    }
+  }
+  // real work
+  if (!joblist.containsJob(jobnum)) {
+    throw STSHException("fg " + to_string(jobnum) + ": No such job.");
+  }
+  STSHJob& job = joblist.getJob(jobnum);
+  assert(job.getState() != kForeground);
+  bool sendSigCont = false;
+  for (auto& p : job.getProcesses()) {
+    if (p.getState() == kStopped) {
+      sendSigCont = true;
+    }
+  }
+  if (sendSigCont) kill(-job.getGroupID(), SIGCONT);
+  job.setState(kForeground);
+  waitForFgJobToFinish();
+}
+
 static bool handleBuiltin(const pipeline& pipeline) {
   const string& command = pipeline.commands[0].command;
   auto iter = find(kSupportedBuiltins, kSupportedBuiltins + kNumSupportedBuiltins, command);
@@ -41,11 +100,38 @@ static bool handleBuiltin(const pipeline& pipeline) {
   switch (index) {
   case 0:
   case 1: exit(0);
+  case 2: fg(pipeline); break;
   case 7: cout << joblist; break;
   default: throw STSHException("Internal Error: Builtin command not supported."); // or not implemented yet
   }
   
   return true;
+}
+
+static void reapChild(int sig) {
+  pid_t pid;
+  while (true) {
+    int status;
+    pid = waitpid(-1, &status, WNOHANG | WUNTRACED | WCONTINUED);
+    if (pid <= 0) break;
+    STSHProcessState state;
+    if (WIFEXITED(status)) {
+      state = kTerminated;
+    } else if (WIFSTOPPED(status)) {
+      state = kStopped;
+    } else if (WIFCONTINUED(status)) {
+      state = kRunning;
+    } else {
+      state = kTerminated; // TODO: more check?
+    }
+    updateJobList(joblist, pid, state);
+  }
+}
+
+static void passSigToFgJob(int sig) {
+  if (joblist.hasForegroundJob()) {
+    kill(-joblist.getForegroundJob().getGroupID(), sig);
+  }
 }
 
 /**
@@ -60,6 +146,9 @@ static void installSignalHandlers() {
   installSignalHandler(SIGQUIT, [](int sig) { exit(0); });
   installSignalHandler(SIGTTIN, SIG_IGN);
   installSignalHandler(SIGTTOU, SIG_IGN);
+  installSignalHandler(SIGCHLD, reapChild);
+  installSignalHandler(SIGINT, passSigToFgJob);
+  installSignalHandler(SIGTSTP, passSigToFgJob);
 }
 
 /**
@@ -68,8 +157,22 @@ static void installSignalHandlers() {
  * Creates a new job on behalf of the provided pipeline.
  */
 static void createJob(const pipeline& p) {
-  cout << p; // remove this line once you get started
-  /* STSHJob& job = */ joblist.addJob(kForeground);
+  pid_t pid = fork();
+  if (pid == 0) {
+    assert(setpgid(0, 0) == 0);
+    command cmd = p.commands[0];
+    char *new_argv[kMaxArguments + 1 + 1];
+    new_argv[0] = cmd.command;
+    for (size_t i = 1; i <= kMaxArguments + 1; i++) {
+      new_argv[i] = cmd.tokens[i - 1];
+      if (cmd.tokens[i - 1] == NULL) break;
+    }
+    execvp(cmd.command, new_argv);
+    exit(1);
+  }
+  STSHJob& job = joblist.addJob(kForeground);
+  job.addProcess(STSHProcess(pid, p.commands[0]));
+  waitForFgJobToFinish();
 }
 
 /**
@@ -82,7 +185,7 @@ static void createJob(const pipeline& p) {
 int main(int argc, char *argv[]) {
   pid_t stshpid = getpid();
   installSignalHandlers();
-  rlinit(argc, argv);
+  rlinit(argc, argv); // configures stsh-readline library so readline works properly
   while (true) {
     string line;
     if (!readline(line)) break;
@@ -99,3 +202,13 @@ int main(int argc, char *argv[]) {
 
   return 0;
 }
+
+// static void addToJobList(STSHJobList& jobList, const vector<pair<pid_t, string>>& children) {
+//   STSHJob& job = jobList.addJob(kBackground);
+//   for (const pair<string, pid_t>& child: children) {
+//     pid_t pid = child.first;
+//     const string& command = child.second;
+//     job.addProcess(STSHProcess(pid, command)); // third argument defaults to kRunning
+//   }
+//   cout << jobList;
+// }
