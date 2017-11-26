cc601d72aa09811b811d79cd23891d01e6ae8ccb
Chuiwen Ma <machuiwen@gmail.com>
2017-11-26 20:21:59 +0000

    Add blacklist and cache

--- assign7/request-handler.cc
+++ assign7/request-handler.cc
@@ -10,55 +10,69 @@
 #include <netdb.h>
 using namespace std;
 
+HTTPRequestHandler::HTTPRequestHandler() throw (HTTPProxyException) {
+  blacklist.addToBlacklist("blocked-domains.txt");
+  cache = HTTPCache();
+}
+
 void HTTPRequestHandler::serviceRequest(const pair<int, string>& connection) throw() {
   const int& clientfd = connection.first;
   const string& clientIPAddress = connection.second;
   sockbuf clientsb(clientfd);
   iosockstream clientStream(&clientsb);
   HTTPRequest request;
+  HTTPResponse response;
   try {
-    request = ingestRequest(clientStream, clientIPAddress);
+    ingestRequest(clientStream, clientIPAddress, request);
   } catch (const HTTPBadRequestException& hbre) {
-    clientStream << createErrorResponse(400, hbre.what());
-    clientStream.flush();
+    sendResponse(clientStream, createErrorResponse(400, hbre.what()));
+    return;
+  }
+  if (!blacklist.serverIsAllowed(request.getServer())) {
+    sendResponse(clientStream, createErrorResponse(403, "Forbidden Content"));
+    return;
+  }
+  if (cache.containsCacheEntry(request, response)) {
+    sendResponse(clientStream, response);
     return;
   }
   int serverfd = createClientSocket(request.getServer(), request.getPort());
   if (serverfd == kClientSocketError) {
-    clientStream << createErrorResponse(404, "Server not found");
-    clientStream.flush();
+    sendResponse(clientStream, createErrorResponse(404, "Server Not Found"));
     return;
   }
   sockbuf serversb(serverfd);
   iosockstream serverStream(&serversb);
-  serverStream << request;
-  serverStream.flush();
-  HTTPResponse response = ingestResponse(serverStream);
-  clientStream << response;
-  clientStream.flush();
+  sendRequest(serverStream, request);
+  ingestResponse(serverStream, response);
+  if (cache.shouldCache(request, response)) {
+    cache.cacheEntry(request, response);
+  }
+  sendResponse(clientStream, response);
 }
 
-// the following two methods needs to be completed 
-// once you incorporate your HTTPCache into your HTTPRequestHandler
-void HTTPRequestHandler::clearCache() {}
-void HTTPRequestHandler::setCacheMaxAge(long maxAge) {}
+void HTTPRequestHandler::clearCache() {
+  cache.clear();
+}
 
-HTTPRequest HTTPRequestHandler::ingestRequest(istream& instream, const string& clientIPAddress) {
-  HTTPRequest request;
+void HTTPRequestHandler::setCacheMaxAge(long maxAge) {
+  cache.setMaxAge(maxAge);
+}
+
+void HTTPRequestHandler::ingestRequest(istream& instream,
+  const string& clientIPAddress, HTTPRequest& request) {
   request.ingestRequestLine(instream);
   request.ingestHeader(instream, clientIPAddress);
   request.ingestPayload(instream);
   request.addHeader("x-forwarded-proto", "http");
   request.addHeader("x-forwarded-for",
     request.getHeaderValueAsString("x-forwarded-for") + "," + clientIPAddress);
-  return request;
 }
 
-HTTPResponse HTTPRequestHandler::ingestResponse(istream& instream) {
-  HTTPResponse response;
+void HTTPRequestHandler::ingestResponse(istream& instream,
+  HTTPResponse& response) {
   response.ingestResponseHeader(instream);
   response.ingestPayload(instream);
-  return response;
 }
 
 HTTPResponse HTTPRequestHandler::createErrorResponse(int code, const string& message) {
@@ -67,4 +81,14 @@ HTTPResponse HTTPRequestHandler::createErrorResponse(int code, const string& mes
   response.setProtocol("HTTP/1.0");
   response.setPayload(message);
   return response;
+}
+
+void HTTPRequestHandler::sendRequest(ostream& outstream, const HTTPRequest& request) {
+  outstream << request;
+  outstream.flush();
+}
+
+void HTTPRequestHandler::sendResponse(ostream& outstream, const HTTPResponse& response) {
+  outstream << response;
+  outstream.flush();
 }
--- assign7/request-handler.h
+++ assign7/request-handler.h
@@ -12,18 +12,26 @@
 #include <string>
 #include "request.h"
 #include "response.h"
+#include "cache.h"
+#include "blacklist.h"
 #include <socket++/sockstream.h> // for sockbuf, iosockstream
 
 class HTTPRequestHandler {
   public:
+    HTTPRequestHandler() throw (HTTPProxyException);
     void serviceRequest(const std::pair<int, std::string>& connection) throw();
     void clearCache();
     void setCacheMaxAge(long maxAge);
 
   private:
-    HTTPRequest ingestRequest(std::istream& instream, const std::string& clientIPAddress);
-    HTTPResponse ingestResponse(std::istream& instream);
+    HTTPBlacklist blacklist;
+    HTTPCache cache;
+
+    void ingestRequest(std::istream& instream, const std::string& clientIPAddress, HTTPRequest& request);
+    void ingestResponse(std::istream& instream, HTTPResponse& response);
     HTTPResponse createErrorResponse(int code, const std::string& message);
+    void sendRequest(std::ostream& outstream, const HTTPRequest& request);
+    void sendResponse(std::ostream& outstream, const HTTPResponse& response);
 };
 
 #endif
